a8810bb885bd83dc2d52d238423c3380287ee512
vinslin <vinslinpackia@gmail.com>
2025-07-16 18:25:47 +0530

    style changed for sidebar static  and button added in the cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -34,6 +34,11 @@ export const routes: Routes = [
       ),
     children: [
       {
+        path: '',
+        redirectTo: 'taskanalytics',
+        pathMatch: 'full',
+      },
+      {
         path: 'taskanalytics',
         loadComponent: () =>
           import(
